729b8cfbf5b3e7ae8348165ff8ec34ca175dcc1e
alexpage5092 <alexpage5092@gmail.com>
2020-03-14 12:09:53 -0700

    Set up
    Set up

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,14 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyAw5y4368FNpfKE7lOh-LjyXrotjjSKifU",
+  authDomain: "album-81780.firebaseapp.com",
+  databaseURL: "https://album-81780.firebaseio.com",
+  projectId: "album-81780",
+  storageBucket: "album-81780.appspot.com",
+  messagingSenderId: "123722583737",
+  appId: "1:123722583737:web:013d31bdf69a1facdfd568",
+  measurementId: "G-JFE60HW855"
 };
 
 firebase.initializeApp(config);
